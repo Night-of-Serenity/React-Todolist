bbe297086e2ff9a8b7358476e231604cff33287a
Nightz <peerapat.ud@gmail.com>
2023-04-25 15:53:24 +0700

    Finish addTodo function

--- src/components/Todo/TodoContent.jsx
+++ src/components/Todo/TodoContent.jsx
@@ -3,11 +3,25 @@ import { TodoHeader } from "./TodoHeader";
 import { TodoList } from "./TodoList";
 import mockData from "../../data/todos.json";
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 export function TodoContent() {
   // Logic
   const [todos, setTodos] = useState(mockData);
-  console.log(todos);
+
+  // Handdle Add Todo
+  const handleAddTodo = () => {
+    // new todo
+    let newTodoObj = { id: uuidv4(), task: "new todo task", status: false, due_date: "" };
+
+    // Create new state
+    // const newTodo = [newTodoObj, ...todos];
+    // Update new state
+    // setTodos(newTodo);q
+
+    // update state โดย callback
+    setTodos((currentState) => [newTodoObj, ...currentState]);
+  };
 
   // Render UI
   return (
@@ -16,6 +30,7 @@ export function TodoContent() {
       <TodoHeader />
       {/* Add Todo */}
       <AddTodo />
+      <button onClick={handleAddTodo}>Test add todo</button>
       {/* TodoList */}
       <TodoList todos={todos} />
     </main>
